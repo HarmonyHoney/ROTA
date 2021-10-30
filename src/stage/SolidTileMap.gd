34bd6cc7940b01b3f66667e762b9221af4f748da
Harmony Honey <HarmonyMonroe97@gmail.com>
2021-10-30 01:01:09 -0400

    AutoTileMap200px! Half Tiles are now 200px up from 50px! (: + player animation transition from run to jump is 0.2! ^.^

--- src/stage/SolidTileMap.gd
+++ src/stage/SolidTileMap.gd
@@ -1,6 +1,6 @@
 extends TileMap
 
-const auto = preload("res://src/stage/AutoTileMap.tscn")
+const auto = preload("res://src/stage/AutoTileMap200px.tscn")
 
 func _ready():
 	get_parent().connect("ready", self, "parent_ready")
@@ -14,18 +14,19 @@ func parent_ready():
 func make_tiles(map : TileMap):
 	for i in get_used_cells():
 		for v in [Vector2.ZERO, Vector2.RIGHT, Vector2.DOWN, Vector2.ONE]:
-			map.set_cellv((i * 2) + v, 3)
+			map.set_cellv((i * 2) + v, 0)
 	
 	var spikes = get_tree().get_nodes_in_group("spike")
 	
 	for i in spikes:
 		for v in [Vector2.ZERO, Vector2.RIGHT, Vector2.DOWN, Vector2.ONE]:
-			map.set_cellv(map.world_to_map(i.position - Vector2(10, 10)) + v, 3)
+			var s = world_to_map(i.position - Vector2(10, 10)) * 2
+			map.set_cellv(s + v, 0)
 	
 	var rect = map.get_used_rect()
 	map.update_bitmask_region(rect.position, rect.end)
 	
 	for i in spikes:
 		for v in [Vector2.ZERO, Vector2.RIGHT, Vector2.DOWN, Vector2.ONE]:
-			map.set_cellv(map.world_to_map(i.position - Vector2(10, 10)) + v, -1)
-		#map.set_cellv(map.world_to_map(i.position), -1)
+			var s = world_to_map(i.position - Vector2(10, 10)) * 2
+			map.set_cellv(s + v, -1)
