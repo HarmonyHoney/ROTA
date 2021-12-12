bdef5cdd68f7ed19ccbce8e18f1b38db2df11fe0
Harmony Honey <HarmonyMonroe97@gmail.com>
2021-12-12 00:35:51 -0500

    autotile working in the editor! with set_cell overridden! (: Solid and Spike maps now autotile! <3

--- src/stage/SolidTileMap.gd
+++ src/stage/SolidTileMap.gd
@@ -1,88 +1,93 @@
+tool
 extends TileMap
 
-onready var auto : TileMap = $Offset
-func _ready():
-	get_parent().connect("ready", self, "parent_ready")
+onready var auto = get_child(0)
 
-func parent_ready():
+func _ready():
+	yield(get_parent(), "ready")
 	tile_set.tile_set_modulate(0, Color(0, 0, 0, 0))
 	make_tiles(auto)
 
+func set_cell(x, y, tile, flip_x=false, flip_y=false, transpose=false, autotile_coord=Vector2()):
+	.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
+	for _x in range(x - 1, x + 2):
+		for _y in range(y - 1, y + 2):
+			set_tile(_x, _y, auto)
+
 func make_tiles(map : TileMap):
 	map.clear()
-	
 	var r = get_used_rect()
-	
 	for x in range(r.position.x - 1, r.position.x + r.size.x + 1):
 		for y in range(r.position.y - 1, r.position.y + r.size.y + 1):
-			var up_left = int(get_cell(x, y) != -1)
-			var up_right = int(get_cell(x + 1, y) != -1)
-			var down_right = int(get_cell(x + 1, y + 1) != -1)
-			var down_left = int(get_cell(x, y + 1) != -1)
-			
-			# Calculate the tile value
-			var tile_value = up_left + (up_right * 2) + (down_right * 4) + (down_left * 8)
-		
-			# Create tile
-			
-			var tile = -1
-			var fx := false
-			var fy := false
-			var tr := false
-			
-			var solid = 0
-			var corner = 1
-			var flat = 2
-			var inside = 3
-			var meeting = 4
-			
-			match tile_value:
-				1:
-					tile = corner
-					fx = true
-					fy = true
-				2:
-					tile = corner
-					fy = true
-				3:
-					tile = flat
-					fy = true
-				4:
-					tile = corner
-				5:
-					tile = meeting
-					fx = true
-				6:
-					tile = flat
-					tr = true
-					fy = true
-				7:
-					tile = inside
-					fy = true
-				8:
-					tile = corner
-					fx = true
-				9:
-					tile = flat
-					tr = true
-					fx = true
-				10:
-					tile = meeting
-				11:
-					tile = inside
-					fx = true
-					fy = true
-				12:
-					tile = flat
-				13:
-					tile = inside
-					fx = true
-				14:
-					tile = inside
-				15:
-					tile = solid
-			
-			map.set_cell(x, y, tile, fx, fy, tr)
+			set_tile(x, y, auto)
 
+func set_tile(x : int, y : int, map):
+	var up_left = int(get_cell(x, y) != -1)
+	var up_right = int(get_cell(x + 1, y) != -1)
+	var down_right = int(get_cell(x + 1, y + 1) != -1)
+	var down_left = int(get_cell(x, y + 1) != -1)
+	
+	# calculate tile value
+	var tile_value = up_left + (up_right * 2) + (down_right * 4) + (down_left * 8)
 
+	# create tile
+	var tile = -1
+	var fx := false
+	var fy := false
+	var tr := false
+	
+	var solid = 0
+	var corner = 1
+	var flat = 2
+	var inside = 3
+	var meeting = 4
+	
+	match tile_value:
+		1:
+			tile = corner
+			fx = true
+			fy = true
+		2:
+			tile = corner
+			fy = true
+		3:
+			tile = flat
+			fy = true
+		4:
+			tile = corner
+		5:
+			tile = meeting
+			fx = true
+		6:
+			tile = flat
+			tr = true
+			fy = true
+		7:
+			tile = inside
+			fy = true
+		8:
+			tile = corner
+			fx = true
+		9:
+			tile = flat
+			tr = true
+			fx = true
+		10:
+			tile = meeting
+		11:
+			tile = inside
+			fx = true
+			fy = true
+		12:
+			tile = flat
+		13:
+			tile = inside
+			fx = true
+		14:
+			tile = inside
+		15:
+			tile = solid
+	
+	if !map: map = get_child(0)
+	map.set_cell(x, y, tile, fx, fy, tr)
 
--- src/stage/SpikeMap.gd
+++ src/stage/SpikeMap.gd
@@ -1,40 +1,60 @@
+tool
 extends TileMap
 
 var spike_scene = preload("res://src/actor/Spike.tscn")
+
+var solid : TileMap
 
 func rot(vec = Vector2.ZERO, dir = 0):
 	return vec.rotated(deg2rad(dir * 90)).round()
 
 func _ready():
+	yield(get_parent(), "ready")
 	tile_set.tile_set_modulate(0, Color(0, 0, 0, 0))
-	
-	var solid = get_parent().get_node("SolidTileMap")
+	solid = get_parent().get_node("SolidTileMap")
+	make_tiles()
+
+func set_cell(x, y, tile, flip_x=false, flip_y=false, transpose=false, autotile_coord=Vector2()):
+	.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
+	if tile == 0:
+		set_spike(Vector2(x, y))
+	else:
+		for i in get_children():
+			if i.position == (Vector2(x, y) + Vector2.ONE / 2) * cell_size:
+				i.queue_free()
+
+func make_tiles():
+	for i in get_children():
+		i.queue_free()
 	
 	for i in get_used_cells():
-		# instance spike
-		var s = spike_scene.instance()
-		add_child(s)
-		
-		# set pos
-		s.position = (i + Vector2.ONE / 2) * cell_size
-		
-		# set dir
-		var t = is_cell_transposed(i.x, i.y)
-		var y = is_cell_y_flipped(i.x, i.y)
-		
-		if t and y:
-			s.dir = 3
-		elif t:
-			s.dir = 1
-		elif y:
-			s.dir = 2
-		
-		# set spike floor
-		var left = i + rot(Vector2(-1, 1), s.dir)
-		var right = i + rot(Vector2(1, 1), s.dir)
-		
-		if get_cellv(left) == -1 and solid.get_cellv(left) == -1:
-			s.set_tile(true)
-		if get_cellv(right) == -1 and solid.get_cellv(right) == -1:
-			s.set_tile(false)
+		set_spike(i)
+
+func set_spike(i : Vector2):
+	# instance spike
+	var s = spike_scene.instance()
+	add_child(s)
+	
+	# set pos
+	s.position = (i + Vector2.ONE / 2) * cell_size
+	
+	# set dir
+	var t = is_cell_transposed(i.x, i.y)
+	var y = is_cell_y_flipped(i.x, i.y)
+	
+	if t and y:
+		s.dir = 3
+	elif t:
+		s.dir = 1
+	elif y:
+		s.dir = 2
+	
+	# set spike floor
+	var left = i + rot(Vector2(-1, 1), s.dir)
+	var right = i + rot(Vector2(1, 1), s.dir)
+	
+	if get_cellv(left) == -1 and solid.get_cellv(left) == -1:
+		s.set_tile(true)
+	if get_cellv(right) == -1 and solid.get_cellv(right) == -1:
+		s.set_tile(false)
 
