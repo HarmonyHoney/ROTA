5abc0d17226d91e7d3471cce85931aaf0ec1e20f
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-02-16 01:06:09 -0500

    guide shader! (:

--- src/actor/Guide.gd
+++ src/actor/Guide.gd
@@ -1,18 +1,31 @@
-extends Line2D
+extends Node2D
 
 var box : Box = null
+
+var open_clock := 0.0
+var open_time := 0.3
+
+onready var sprite := $Sprite
+
+var is_deploy := false
 
 func _ready():
 	visible = false
 
 func _physics_process(delta):
+	open_clock = clamp(open_clock + (delta if is_deploy else -delta), 0, open_time)
+	
+	visible = open_clock > 0
 	if visible:
-		place()
+		var s = smoothstep(0, 1, open_clock / open_time)
+		sprite.scale.x = s
+		if is_instance_valid(box):
+			place()
 
 func set_box(b):
-	box = b
-	visible = is_instance_valid(box)
-	if visible:
+	is_deploy = is_instance_valid(b)
+	if is_deploy:
+		box = b
 		place()
 
 func place():
