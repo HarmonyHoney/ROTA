ecd366dbbb63935e67ecf5ab55fdb87beaa3526f
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-02-19 15:33:13 -0500

    Node2D hair!!

--- src/actor/Hair.gd
+++ src/actor/Hair.gd
@@ -1,23 +1,22 @@
 tool
-extends Line2D
+extends Node2D
 
+export var width := 50.0
+export var end_scale := 0.75
 export var length = 25
+export var point_count := 3 setget _set_points
+
 export var gravity = 200.0
 export var sitting_angle = 15.0
 export var lerp_weight := 0.3
-
-export var point_count := 10 setget _set_points
 export var reload := false setget set_reload
 
-#export var radius = 53.0
-#export var color = Color("ff007e")
-
-#export var points := 10
-
-
 var hair_end = Vector2(-150, 150)
-var last_pos : Vector2 = position
+var last_pos := Vector2.ZERO
 
+var p = []
+
+export var color := Color("00fff9") # 00fff9 ff007e
 
 func _physics_process(delta):
 	
@@ -38,13 +37,13 @@ func _physics_process(delta):
 	if hair_end.length() > length:
 		hair_end = hair_end.normalized() * length
 	
-	for i in points.size():
-		points[i] = hair_end.normalized() * hair_end.length() * (float(i) / (points.size() - 1))
+	# set points
+	for i in p.size():
+		p[i] = hair_end.normalized() * hair_end.length() * (float(i) / (p.size() - 1))
 	
-	#points[1] = hair_end.normalized() * hair_end.length() * 0.5
-	points[points.size() - 1] = hair_end
 	
 	last_pos = to_global(hair_end)
+	update()
 
 func set_reload(arg):
 	reload = arg
@@ -53,11 +52,17 @@ func set_reload(arg):
 func _set_points(arg := point_count):
 	point_count = max(2, arg)
 	
-	clear_points()
-	
+	p = []
 	for i in point_count:
-		add_point(Vector2.ZERO)
+		p.append(Vector2.ZERO)
 	
+	update()
+
+func _draw():
+	for i in p.size():
+		var w = (width * 0.5) * lerp(1.0, end_scale, i / float(p.size() - 1))
+		draw_circle(p[i], w, color) 
 	
-
-
+	if Engine.editor_hint:
+		for i in p.size():
+			draw_circle(p[i], 1, Color.white)
