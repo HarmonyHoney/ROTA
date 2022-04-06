32ff7e980ffb67eb77dea5e0436012bf953adc03
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-04-05 21:11:16 -0400

    grid! (:

--- src/autoload/Options/RemapMenu.gd
+++ src/autoload/Options/RemapMenu.gd
@@ -7,33 +7,31 @@ onready var list_node := $Control/List
 onready var items_node := $Control/List/Items
 onready var items := items_node.get_children()
 
-var cursor := 0 setget set_cursor
-var scroll := 0
-var cursor_x := 0 setget set_cursor_x
-
-var start := Vector2.ZERO
-var positions := []
-var cursor_margin := Vector2(-50, 0)
-
 var is_open := false
+
+var scroll := 0
+var cursor := 0 setget set_cursor
+var cursor_x := 0 setget set_cursor_x
+export var cursor_margin := Vector2(-50, 0)
 
 var joy := Vector2.ZERO
 var joy_last := Vector2.ZERO
 
+var grid := {}
+
 func _ready():
 	show(false)
 	
-	var h = $Control/List/Items/Up/HBoxContainer
+	cursor_node.rect_size = $Control/List/Items/Up/HBoxContainer.get_child(0).rect_size
 	
-	start = h.rect_global_position
+	# fill grid dict
+	for y in items.size():
+		var hb = items[y].get_node("HBoxContainer")
+		
+		for x in hb.get_child_count():
+			grid[Vector2(x, y)] = hb.get_child(x)
 	
-	for i in h.get_children():
-		positions.append(i.rect_position)
-	
-	cursor_node.rect_size = h.get_child(0).rect_size
-	
-	
-	
+	print(grid)
 
 func _input(event):
 	if !is_open: return
@@ -54,17 +52,17 @@ func _input(event):
 		show(false)
 
 func _physics_process(delta):
+	var target = grid[Vector2(cursor_x, cursor)]
 	
-	var c = cursor_node.rect_position
-	var l = list_node.rect_position
-	var i = items[cursor - scroll].rect_position
+	# position
+	var cg = cursor_node.rect_global_position
+	cg = cg.linear_interpolate(target.rect_global_position - cursor_margin, 0.15)
+	cursor_node.rect_global_position = cg
 	
-	c.x = lerp(c.x, start.x + positions[cursor_x].x , 0.15)
-	c.y = lerp(c.y, l.y + i.y, 0.15)
-	
-	cursor_node.rect_position = c
-	
-	
+	# size
+	var cs = cursor_node.rect_size
+	cs = cs.linear_interpolate(target.rect_size + (cursor_margin * 2.0), 0.15)
+	cursor_node.rect_size = cs
 	
 	# scroll
 	items_node.rect_position = items_node.rect_position.linear_interpolate(Vector2(0, 80) * -scroll, 0.15)
