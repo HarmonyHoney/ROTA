e2fbcb6f63ef1d1ea6b7c1c4182a75395e649f3e
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-04-28 17:22:38 -0400

    using get_node_or_null() on MenuBase cleanup! (:

--- src/menu/MenuBase.gd
+++ src/menu/MenuBase.gd
@@ -3,21 +3,18 @@ class_name MenuBase
 
 var items = []
 export var items_path : NodePath
-onready var items_node = get_node(items_path)
+onready var items_node = get_node_or_null(items_path)
 
 export var cursor_path : NodePath
-onready var is_cursor : bool = cursor_path != ""
-onready var cursor_node = get_node(cursor_path) if cursor_path else null
+onready var cursor_node = get_node_or_null(cursor_path)
 var cursor := 0 setget set_cursor
 export var cursor_margin := Vector2(30, 0)
 
 export var scroll_path : NodePath
-onready var is_scroll : bool = scroll_path != ""
-onready var scroll_node = get_node(scroll_path) if scroll_path else null
+onready var scroll_node = get_node_or_null(scroll_path)
 
 export var fade_path : NodePath
-onready var is_fade = fade_path != ""
-onready var fade_node = get_node(fade_path) if fade_path else null
+onready var fade_node = get_node_or_null(fade_path)
 onready var fade_ease := EaseMover.new()
 
 export var is_open := false setget set_open
@@ -41,7 +38,7 @@ func _ready():
 	reset_cursor()
 
 func menu_input(event):
-	if !is_open or is_sub_menu or (is_fade and fade_ease.frac() < 0.4): return
+	if !is_open or is_sub_menu or (fade_node and fade_ease.frac() < 0.4): return
 	
 	var accept = event.is_action_pressed("ui_accept")
 	var back = event.is_action_pressed("ui_cancel")
@@ -69,7 +66,7 @@ func menu_input(event):
 			joy_x(joy.x)
 
 func menu_process(delta):
-	if is_fade:
+	if fade_node:
 		fade_node.modulate.a = fade_ease.count(delta, is_open)
 		fade_node.visible = fade_ease.clock > 0
 	
@@ -82,24 +79,24 @@ func menu_process(delta):
 				self.cursor += joy.x if axis_x else joy.y
 		
 		# move cursor
-		if is_cursor:
+		if items_node and cursor_node:
 			cursor_node.rect_global_position = cursor_node.rect_global_position.linear_interpolate(items[cursor].rect_global_position - cursor_margin, 0.15)
 			cursor_node.rect_size = cursor_node.rect_size.linear_interpolate(items[cursor].rect_size + (cursor_margin * 2.0), 0.15)
 			
 			# scroll
-			if is_scroll:
+			if scroll_node:
 				scroll_node.rect_position.y = lerp(scroll_node.rect_position.y, (720 / 2.0) - (cursor_node.rect_position.y + cursor_node.rect_size.y / 2.0), 0.08)
 
 func set_cursor(arg := 0):
 	cursor = clamp(arg, 0, max(items.size() - 1, 0))
 
 func reset_cursor():
-	if is_cursor:
+	if items_node and cursor_node:
 		cursor_node.rect_global_position = items[0].rect_global_position - cursor_margin
 		cursor_node.rect_size = items[0].rect_size + (cursor_margin * 2.0)
 		
 		# scroll
-		if is_scroll:
+		if scroll_node:
 			scroll_node.rect_position.y = (720 / 2.0) - (cursor_node.rect_position.y + cursor_node.rect_size.y / 2.0)
 
 func set_open(arg := is_open):
