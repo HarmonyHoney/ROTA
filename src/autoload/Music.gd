4d743b4390f860628c2d19187daa490ab851e18f
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-04-29 19:23:42 -0400

    ost 3 + 4 and random playlist function!(: <3

--- src/autoload/Music.gd
+++ src/autoload/Music.gd
@@ -2,13 +2,35 @@ extends AudioStreamPlayer
 
 export var ost1 : AudioStream
 export var ost2: AudioStream
+export var ost3 : AudioStream
+export var ost4 : AudioStream
+
+var last_song := 3
+var array = []
 
 func _ready():
-	stream = ost2
-	play()
-
+	randomize()
+	play_song()
 
 func _on_Music_finished():
-	stream = ost2 if stream == ost1 else ost1
+	play_song()
+
+func play_song():
+	if array.size() == 0:
+		array = [0,1,2,3]
+		array.erase(last_song)
+		array.shuffle()
 	
+	match array[0]:
+		0:
+			stream = ost1
+		1:
+			stream = ost2
+		2:
+			stream = ost3
+		3:
+			stream = ost4
+	
+	last_song = array[0]
+	array.remove(0)
 	play()
