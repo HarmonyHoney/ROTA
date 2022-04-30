cf2b4c44cd79388148f17d5fad259171c997423a
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-04-30 17:30:05 -0400

    Image resizing and cleanup!(: Some images now import with mipmaps or anisotropic!

--- src/detail/Fence.gd
+++ src/detail/Fence.gd
@@ -4,13 +4,14 @@ extends Node2D
 export var length := 3 setget set_length
 export var is_offset := false setget set_offset
 
+var width := 100
 
 func set_length(arg):
 	length = max(1, abs(arg))
 	if $Sprite:
-		$Sprite.region_rect.size.x = 200 * length
+		$Sprite.region_rect.size.x = width * length
 
 func set_offset(arg : bool):
 	is_offset = arg
 	if $Sprite:
-		$Sprite.region_rect.position.x = 200 * int(is_offset)
+		$Sprite.region_rect.position.x = width * int(is_offset)
--- src/detail/Flower.gd
+++ src/detail/Flower.gd
@@ -1,15 +1,2 @@
 tool
 extends PaletteSwap
-
-export var is_pot := false setget set_pot
-# colors ["FFF", "FF0000", "FF78CB", "79FFFF", "FFFA00", "FFC900"]
-
-func set_pot(arg : bool):
-	is_pot = arg
-	
-	if $Sprites and $Area2D:
-		$Sprites/Flower.position.y = -150 * int(is_pot)
-		$Sprites/Pot.visible = is_pot
-		$Area2D.position.y = -45 if is_pot else -25
-	
-	z_index = 1 if is_pot else -10
