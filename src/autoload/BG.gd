820ee8c8d2aca38b27a783753bd7dbe6742f346d
Harmony Honey <HarmonyMonroe97@gmail.com>
2023-03-06 17:22:36 -0500

    sun and moon! (-=

--- src/autoload/BG.gd
+++ src/autoload/BG.gd
@@ -5,18 +5,8 @@ onready var mat : ShaderMaterial = $ColorRect.material
 export var colors = 0 setget set_colors
 export(PoolColorArray) var palette : PoolColorArray = ["0062ff", "00eaff", "af00bf", "0079db", "ff00ff", "009dff", "00eaff", "0062ff", "6666ff", "00004d"]
 
-onready var center := $Center
-onready var sun := $Center/Sun
-export var rot_speed := 100.0
-
-
 func _ready():
 	self.colors = 0
-
-func _physics_process(delta):
-	if Engine.editor_hint: return
-	
-	if center: center.rotate(deg2rad(rot_speed * delta))
 
 func set_colors(arg := 0):
 	colors = clamp(arg, 0, (palette.size() / 2) - 1)
--- src/autoload/Clouds.gd
+++ src/autoload/Clouds.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
-onready var center := $Center
 onready var hide := $Hide
 onready var circle := $Hide/Circle
 onready var color_rect := $Hide/ColorRect
 
-export var rot_speed := 50.0
+onready var center := $Center
+onready var clouds := $Center/Clouds
+onready var stars := $Center/Stars
+onready var sun := $Center/Stars/Sun
+onready var moon := $Center/Stars/Moon
+
+export var cloud_speed := 1.0
+export var star_speed := 0.5
+var cloud_dir := 1.0
+var star_dir := 1.0
+
 var length = 100.0
 var dir := 1.0
 var speed_mod := 1.0
@@ -32,25 +41,34 @@ func scene():
 	color_rect.rect_position = -color_rect.rect_size / 2.0
 	length = max(color_rect.rect_size.x, color_rect.rect_size.y) / 2.0
 	
+	sun.position = Vector2(length + 750, 0)
+	moon.position = sun.position
+	sun.visible = BG.colors == 0 or BG.colors == 3
+	moon.visible = !sun.visible
+	stars.rotation = rand_range(0.0, TAU)
+	
 	print("start: ", start, " end: ", end, " length: ", length, " global_position: ", global_position)
 	
-	dir = -1.0 if randf() > 0.5 else 1.0
-	speed_mod = rand_range(0.6, 1.0) * dir
+	cloud_dir = (-1.0 if randf() > 0.5 else 1.0) * rand_range(0.6, 1.0)
+	star_dir = (-1.0 if randf() > 0.5 else 1.0) * rand_range(0.6, 1.0)
+	#speed_mod = rand_range(0.6, 1.0) * dir
 	create_clouds()
 
 func _physics_process(delta):
-	center.rotate(deg2rad(rot_speed * delta * speed_mod))
+	clouds.rotate(deg2rad(cloud_speed * delta * cloud_dir))
+	stars.rotate(deg2rad(star_speed * delta * -star_dir))
 
 func create_clouds():
-	for i in center.get_children():
+	for i in clouds.get_children():
 		i.queue_free()
 	
 	for x in 20:
 		for y in 3 + x:
 			var c = circle.duplicate()
-			center.add_child(c)
-			c.owner = center
+			clouds.add_child(c)
+			c.owner = clouds
 			c.position = Vector2((x + 2) * 200, 0).rotated(rand_range(0.0, TAU))
 			if c.position.length() > length + 500:
+				#c.color.a = lerp(c.color.a, 1.0, clamp((c.position.length() - (length + 700)) / 500.0, 0, 1))
 				c.color.a = 0.8
 		
