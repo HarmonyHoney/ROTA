903acc41df3d255a871c4c7ffe3459a3f888071c
Harmony Honey <HarmonyMonroe97@gmail.com>
2023-03-11 16:08:33 -0500

    rain and snow! small chance for rain <3 (=

--- src/autoload/Clouds.gd
+++ src/autoload/Clouds.gd
@@ -10,7 +10,7 @@ onready var clouds := $Center/Clouds
 onready var stars := $Center/Stars
 onready var sun := $Center/Stars/Sun
 onready var moon := $Center/Stars/Moon
-onready var snow := $Center/Snow
+onready var precip := $Center/Fall
 
 export var cloud_speed := 1.0
 export var star_speed := 0.5
@@ -24,11 +24,18 @@ var speed_mod := 1.0
 var clock := 0.0
 var step := 1.0
 
+export var snow_mat : ParticlesMaterial
+export var snow_tex : Texture
+export var rain_mat : ParticlesMaterial
+export var rain_tex : Texture
+
 func _enter_tree():
 	Shared.connect("scene_changed", self, "scene")
 
 func _ready():
 	hide.visible = false
+	particles.emitting = false
+	particles.visible = false
 
 func scene():
 	var start = Vector2.ONE * 900
@@ -60,26 +67,29 @@ func scene():
 	#speed_mod = rand_range(0.6, 1.0) * dir
 	create_clouds()
 	yield(get_tree(), "physics_frame")
-	solve_snow()
+	solve_fall()
 
 func _physics_process(delta):
 	clouds.rotate(deg2rad(cloud_speed * delta * cloud_dir))
-	snow.rotation = clouds.rotation
+	precip.rotation = clouds.rotation
 	stars.rotate(deg2rad(star_speed * delta * -star_dir))
 	
 	clock += delta
 	if clock > step:
 		clock -= step
-		solve_snow()
+		solve_fall()
 
 func create_clouds():
 	var ci = -1
 	var pi = -1
 	var gc = clouds.get_children()
 	var gcs = gc.size()
-	var pc = snow.get_children()
+	var pc = precip.get_children()
 	var pcs = pc.size()
 	var is_snow = "2A/" in Shared.csfn or "3B" in Shared.csfn
+	var is_rain = (randf() > 0.8) and (Shared.map_name != "")
+	print("smn: ", Shared.map_name, " is_rain: ", is_rain, " is_snow: ", is_snow)
+	
 	
 	for x in (length / 50.0) + 5:
 		for y in max(3, x):
@@ -104,37 +114,44 @@ func create_clouds():
 				#c.color.a = lerp(c.color.a, 1.0, clamp((cpl - (length + 700)) / 500.0, 0, 1))
 				c.color.a = 0.8
 				
-				if is_snow and cpl < length + 1000:
+				if cpl < length + 1000:
 					pi += 1
 					var p = null
 					if pi < pcs:
 						p = pc[pi]
 					else:
 						p = particles.duplicate()
-						snow.add_child(p)
-						p.owner = snow
-						p.process_material = p.process_material.duplicate()
+						precip.add_child(p)
+						p.owner = precip
+						#p.process_material = p.process_material.duplicate()
+					
+					p.process_material = (snow_mat if is_snow else rain_mat).duplicate()
+					p.texture = snow_tex if is_snow else rain_tex
+					p.amount = c.scale.x * (200 if is_snow else 100)
+					p.lifetime = 9.0 if is_snow else 3.0
 					
 					p.position = c.position
 					p.rotation = angle + (PI/2.0)
 					p.process_material.emission_sphere_radius = c.scale.x * 150.0
-					p.amount = c.scale.x * 200
-					p.visible = true
-					p.emitting = true
+					
+					p.visible = is_snow or is_rain
+					p.emitting = is_snow or is_rain
 	
+	# clouds
 	if ci < gcs:
 		for a in range(max(ci, 0), gcs):
 			gc[a].visible = false
+	# particles
 	if pi < pcs:
 		for a in range(max(pi, 0), pcs):
 			pc[a].visible = false
 			pc[a].emitting = false
 
 
-func solve_snow():
-	for i in snow.get_children():
+func solve_fall():
+	for i in precip.get_children():
 		var r = i.get_child(0)
 		var rp = r.get_collision_point()
 		var d = r.global_position.distance_to(rp)
 		#print(i.name, " ", d)
-		i.lifetime = (d / 50.0)
+		i.lifetime = (d / i.process_material.initial_velocity)
